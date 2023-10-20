bc839cff04ff2f31f57d5534efdde4f83fc929ae
Luciel707 <juanramosxd11@gmail.com>
2023-10-20 10:26:22 -0400

    Update datos

--- Proy_MySQL/datos_veterinaria.sql
+++ Proy_MySQL/datos_veterinaria.sql
@@ -386,3 +386,275 @@ insert into provee(id_Producto, id_Proveedor, cantidad) values
 (32, 125, 70),
 (31, 124, 35);
 
+# Gabriel - datos a insertar
+#==============================================================================================================
+# persona
+insert into persona(ci, nombres, apepaterno, apematerno, sexo, fecha_de_nacimiento, correo, telefono) values
+(2431234, 'Sheyla', 'Perez', 'Copa', 'F', '1998-06-22', 'sheyla@gmail.com', 72837457),
+(2354237, 'Winder', 'Canezo', 'Gutierrez', 'M', '1980-02-21', 'wcanezo1@gmail.com', 68485732),
+(2567457, 'Eleazar', 'Cruz', 'Pari', 'M', '1999-02-21', 'Elecr@gmail.com', 67374722),
+(2415428, 'Ruth', 'Usnayo', 'Valdez', 'F', '2001-10-01', 'Ruth121@gmail.com', 78364525),
+(2664223, 'Amira', 'Perez', 'Mayta', 'F', '200-05-20', 'Perezamira123@gmail.com', 77785764),
+(2536993, 'Ariel', 'Camacho', 'Ruiz', 'M', '1985-10-06', 'Aricamacho76@gmail.com', 76494375);
+
+#medico
+
+insert into medico(ciMedico, especialidad, sueldo) values
+(2431234, 'Medicina Interna Veterinaria', 8500),
+(2415428, 'Dermatología Veterinaria', 6300),
+(2567457, 'Oftalmología Veterinaria', 7600);
+
+#dueño
+insert into dueño(ciDueño, direccion) values
+(2536993, 'Avenida René Zabaleta #85'),
+(2664223, 'Calle Jaén #500'),
+(2415428, 'Calle Sagárnaga #315'),
+(2567457, 'Avenida Illampu #765');
+
+#mascota
+insert into mascota(idMascota, nombre, especie, raza, fecha_de_nacimiento, ciDueño) values
+(36, 'Yogui', 'Perro', 'Mestizo', '2021-11-10', 2536993),
+(37, 'Perla', 'Perro', 'Pitbul', '2010-07-21', 2664223),
+(38, 'Bobi', 'Perro', 'Chihuahua', '2022-03-12', 2415428),
+(39, 'Copito', 'Gato', 'Siberiano', '2019-11-23', 2567457),
+(40, 'Sussy', 'Gato', 'Maine Coon', '2023-01-20', 2536993);
+
+
+#atiende
+insert into atiende(idMascota, ciMedico) values
+(36, 2431234),
+(37, 2431234),
+(38, 2567457),
+(39, 2415428),
+(40, 2567457);
+
+#diagnostico
+insert into diagnostico(id_Diagnostico, fecha_de_diagnostico, diagnostico, tratamiento, observaciones) values
+(216, '2023-03-01', 'Infección en el oído derecho', 'Limpieza del oído y prescripción de antibióticos tópicos', 'Se instruye al propietario sobre la administración de medicamentos y la importancia de mantener el oído limpio y seco'),
+(217, '2023-03-10', 'Alergia a las pulgas', 'Tratamiento antipulgas, medicamentos antihistamínicos y champú especial', 'Se aconseja al dueño sobre la prevención de pulgas y cuidados continuos de la piel'),
+(218, '2023-04-15', 'Radiografía que revela una fractura en la pata trasera del perro', 'Reducción de la fractura y colocación de un yeso', 'El propietario debe restringir la actividad del perro durante la recuperación '),
+(219, '2023-04-21', 'Análisis de orina muestra cristales en la orina del gato', 'Cirugía de extirpación y radioterapia', 'Se recomienda una dieta específica y se programan chequeos regulares'),
+(220, '2023-05-13', 'Alergia a las pulgas', 'Tratamiento antipulgas, medicamentos antihistamínicos y champú especial', 'Se aconseja al dueño sobre la prevención de pulgas y cuidados continuos de la piel'),;
+
+
+#asigna
+insert into asigna(idMascota, ciMedico, id_Diagnostico) values
+(36, 2431234, 217),
+(37, 2431234, 218),
+(38, 2567457, 220),
+(39, 2415428, 216),
+(40, 2567457, 219);
+
+#receta
+insert into receta(id_Diagnostico, id_Producto) values
+(216, 24),
+(216, 25), 
+(217, 26),
+(218, 27),
+(219, 28),
+(220, 26);
+
+#producto
+insert into producto(id_Producto, nombre, descripcion, precio, stock) values
+(24, 'Gotas Antibióticas', 'Oticas antibióticas para tratar la infección bacteriana en el oído de tu gato', 45.67, 70),
+(25, 'Analgesia', 'Aliviar el dolor y la incomodidad', 25.50, 150),
+(26, 'Antihistamínicos', 'Ayuda a aliviar los síntomas alérgicos en perros', 23.45, 45),
+(27, 'Antiinflamatorios no esteroides', 'Medicamentos como el carprofeno, el meloxicam o el firocoxib se utilizan para reducir la inflamación ', 27.50, 100),
+(28, 'Medicamentos antiinflamatorios', 'Si los cristales están causando irritación o inflamación en el tracto urinario del gato', 23.60, 50);
+  
+#proveedor
+insert into proveedor(id_Proveedor, nombreEmpresa, telefono, correo) values
+(169, 'Veterquimica Bolivia', 78876534, 'veterquimBol@gmail.com'),
+(170, 'Agrovet Market', 65764564, 'AgrovetMarket@gmail.com'),
+(171, 'AnimaLove', 77857645, 'AnimaLove@gmail.com'),
+(172, 'Faunamed', 66236475, 'Faunamed@gmail.com');
+
+
+#provee
+insert into provee(id_Producto, id_Proveedor, cantidad) values
+(24, 169, 60),
+(25, 170, 40),
+(26, 171, 95),
+(27, 172, 70),
+(28, 115, 80);   
+
+# datos a insertar
+# ALEXIS
+
+insert into persona(ci, nombres, apepaterno, apematerno, sexo, fecha_de_nacimiento, correo, telefono) values
+(1234567, 'Laura', 'Lopez', 'Gonzalez', 'F', '2000-09-05', 'laura@gmail.com', '77788899'),
+(7654321, 'Daniel', 'Ramirez', 'Martinez', 'M', '1995-11-17', 'daniel@hotmail.com', '66655544'),
+(9876543, 'Alejandra', 'Cortez', 'Suarez', 'F', '1992-03-20', 'alejandra@yahoo.com', '55544433'),
+(5555555, 'Andres', 'Gomez', 'Rodriguez', 'M', '1998-08-01', 'andres@outlook.com', '88899900'),
+(8888888, 'Carla', 'Martinez', 'Lopez', 'F', '2002-06-15', 'carla@gmail.com', '99900011'),
+(9876540, 'Karla', 'García', 'Mendoza', 'F', '1993-12-08', 'karla@gmail.com', '78945632'),
+(5555556, 'Javier', 'Martinez', 'Gutierrez', 'M', '1997-05-15', 'javier@hotmail.com', '66995588'),
+(8888889, 'Verónica', 'Lopez', 'Hernandez', 'F', '1988-09-21', 'veronica@yahoo.com', '55511100'),
+(1234568, 'Alejandro', 'Perez', 'Sanchez', 'M', '2001-03-25', 'alejandro@outlook.com', '77722233');
+
+insert into medico(ciMedico, especialidad, sueldo) values
+(1234567, 'Dermatología Veterinaria', 4800),
+(7654321, 'Cirugia Veterinaria', 5800),
+(9876543, 'Cardiología Veterinaria', 5200),
+(5555555, 'Oncología Veterinaria', 5000),
+
+insert into dueño(ciDueño, direccion) values
+(8888888, 'Calle 123 #456'),
+(9876540, 'Av. Bolivar #567'),
+(5555556, 'Calle Sucre #789'),
+(8888889, 'Av. America #890'),
+(1234568, 'Calle Comercio #1234');
+
+insert into mascota(idMascota, nombre, especie, raza, fecha_de_nacimiento, ciDueño) values
+(26, 'Max', 'Perro', 'Golden Retriever', '2018-04-10', 8888888),
+(27, 'Mia', 'Gato', 'Siamés', '2020-02-15', 9876540),
+(28, 'Rocky', 'Perro', 'Labrador', '2017-07-20', 5555556),
+(29, 'Bella', 'Gato', 'Persa', '2019-10-05', 8888889),
+(40, 'Lucy', 'Perro', 'Poodle', '2016-06-30', 1234568);
+
+insert into atiende(idMascota, ciMedico) values
+(26, 1234567),
+(27, 7654321),
+(28, 9876543),
+(29, 5555555),
+(40, 5555555);
+
+insert into diagnostico(id_Diagnostico, fecha_de_diagnostico, diagnostico, tratamiento, observaciones) values
+(221, '2023-08-10', 'Fractura de pata', 'Inmovilización y seguimiento radiográfico', 'Revisión en 6 semanas'),
+(222, '2023-09-05', 'Infección de oído', 'Limpieza y tratamiento con antibióticos', 'Volver en 2 semanas para control'),
+(223, '2023-10-12', 'Problemas gastrointestinales', 'Dieta especial y medicamentos', 'Revisión en 4 semanas'),
+(224, '2023-11-15', 'Problemas dentales', 'Limpieza dental y extracción de dientes', 'Cuidado dental regular necesario'),
+(225, '2023-09-21', 'Problemas de comportamiento', 'Entrenamiento y terapia conductual', 'Revisión en 8 semanas para evaluar el progreso');
+
+insert into asigna(idMascota, ciMedico, id_Diagnostico) values
+(26, 1234567, 221),
+(27, 7654321, 222),
+(28, 9876543, 223),
+(29, 5555555, 224),
+(40, 5555555, 225);
+
+insert into producto(id_Producto, nombre, descripcion, precio, stock) values
+(50, 'Antibiótico', 'Medicamento antibiótico de amplio espectro', 15.99, 50),
+(51, 'Antiinflamatorio', 'Medicamento para reducir la inflamación', 20.50, 40),
+(52, 'Analgesia', 'Medicamento para el alivio del dolor', 12.75, 60),
+(53, 'Vitaminas para Mascotas', 'Suplemento vitamínico para mascotas', 10.99, 75),
+(54, 'Antiparasitario', 'Medicamento para el control de parásitos', 18.25, 30);
+
+insert into receta(id_Diagnostico, id_Producto) values
+(221, 50),
+(222, 51),
+(223, 52),
+(224, 53),
+(225, 54);
+
+insert into proveedor(id_Proveedor, nombreEmpresa, telefono, correo) values
+(61, 'Mascotas Felices', 78889900, 'contacto@mascotasfelices.com'),
+(62, 'Productos para Mascotas S.A.', 76665544, 'info@productosparamascotas.com'),
+(63, 'Veterinaria Amiga', 74443322, 'ventas@veterinariaamiga.com'),
+(64, 'Mascotas Online', 78881111, 'info@mascotasonline.com'),
+(65, 'Animal Saludable', 75554433, 'atencion@animalsaludable.com');
+
+insert into provee(id_Producto, id_Proveedor, cantidad) values
+(50, 61, 40),
+(51, 62, 30),
+(52, 63, 25),
+(53, 64, 50),
+(54, 65, 35);
+
+
+-- ERICKA
+
+#datos a insertar
+
+#persona
+insert into persona(ci, nombres, apepaterno, apematerno, sexo, fecha_de_nacimiento, correo, telefono) values
+(4021781, 'Hans Gelmer', 'Huanaco', 'Caceres', 'M', '2002-10-17', 'gelmi86@gmail.com', 60001178),
+(6962120, 'Erika Cecilia', 'Averanga', 'Ali', 'F', '2000-06-10', 'ceci16@gmail.com', 68095331),
+(4984222, 'Valerio Vidal', 'Ramos', 'Uyuli', 'M', '1998-12-25', 'valvid45@gmail.com', 72549833),
+(3358411, 'Carlos Lorenzo', 'Ayare', 'Orellana', 'M', '1999-09-09', 'carloren123gmail.com', 74829116),
+(5291354, 'Carmen Rosa', 'Lizarazu', 'Padilla', 'F', '1997-01-42', 'rosita456@gmail.com', 68099426),
+(3045707, 'Nilo Justiniano', 'Suxo', 'Nina', 'M', '1999-11-03', 'justinil888@gmail.com', 71244212),
+(1276183, 'Fernando', 'Paucara', 'Perez', 'M', '1998-03-19', 'pepefer@gmail.com', 78644561),
+(8793155, 'Lourdes Diana', 'Mendez', 'Duran', 'F', '2001-01-16', 'lulidi1@gmail.com', 67430101);
+
+#medico
+insert into medico(ciMedico, especialidad, sueldo) values
+(8793155, 'Rehabilitacion Veterinaria', 7000),
+(3358411, 'Medicina Interna', 6000),
+(1276183, 'Imagenologia Veterinaria', 5000);
+
+#dueño
+insert into dueño(ciDueño, direccion) values
+(4021781, 'Av. Arce #123'),
+(6962120, 'Calle Paz Nery Nava #630'),
+(4984222, 'Landaeta #432'),
+(5291354, 'La Portada #555'),
+(3045707, 'Calle 23 de Clacoto #776');
+
+####-
+#atiende
+insert into atiende(idMascota, ciMedico) values
+(55, 3358411),
+(56, 8793155),
+(57, 1276183),
+(58, 3358411),
+(59, 1276183);
+
+#mascota
+insert into mascota(idMascota, nombre, especie, raza, fecha_de_nacimiento, ciDueño) values
+(55, 'mimi', 'perro', 'chihuahua', '2017-03-11', 4021781),
+(56, 'dante', 'gato', 'siames', '2020-10-31', 6962120),
+(57, 'coki', 'perro', 'border collie', '2023-05-05', 4984222),
+(58, 'chimera', 'perro', 'bichon maltes', '2021-07-10', 5291354),
+(59, 'lily', 'gato', 'persa', '2016-09-21', 3045707);
+####-
+
+#asigna
+insert into asigna(idMascota, ciMedico, id_Diagnostico) values
+(55, 3358411, 551),
+(56, 8793155, 552),
+(57, 1276183, 553),
+(58, 3358411, 554),
+(59, 1276183, 555);
+
+#diagnostico
+insert into diagnostico(id_Diagnostico, fecha_de_diagnostico, diagnostico, tratamiento, observaciones) values
+(551, '2023-05-23', 'Otitis', 'Se le recetaron unas gotas para el oido', 'Si no mejora en una semana revisar a profundidad'),
+(552, '2023-09-21', 'Picadura de abeja', 'Se le receto una crema desinflamante', 'Si no baja la inflamacion en 24 hrs volver para revision'),
+(553, '2022-07-11', 'Gastritis', 'Se le recetaron pastillas para la gastritis y dieta especial', 'Mantener la dieta por 2 meses'),
+(554, '2022-10-01', 'Obesidad', 'Implementación de una dieta y programa de ejercicios', 'Cita de control en una semana'),
+(555, '2022-03-10', 'Dermatitis Atópica', 'Se le receto shampoo para mantos delicados', 'Cita de control en dos semanas');
+###-
+
+#receta
+insert into receta(id_Diagnostico, id_Producto) values
+(551, 33),
+(552, 34),
+(553, 35),
+(554, 36),
+(555, 37);
+
+#producto
+insert into producto(id_Producto, nombre, descripcion, precio, stock) values
+(33, 'EasOtic', 'Gotas para la otitits', 100, 5),
+(34, 'Inflavet', 'Crema desinflamante', 40.99, 23),
+(35, 'Famotidina', 'Pastillas para la gastritis', 2.50, 100),
+(36, 'Proplan Bajo Calorias Optifi', 'Croquetas 50 kg', 150.03, 7),
+(37, 'Tacrolimus', 'Crema para la dermatitis atopica', 239.99, 20);
+
+#proveedor
+insert into proveedor(id_Proveedor, nombreEmpresa, telefono, correo) values
+(201, 'Dogtisto', 66668787, 'dogtistofeli@gmail.com'),
+(202, 'Mascotita Mimada', 77724242, 'mimaditos333@gmail.com'),
+(203, 'Cutie Pets', 67676789, 'petsc432@gmail.com'),
+(204, 'Best buddy', 77889910, 'lovepets55@gmail.com'),
+(205, 'Cuidados con amor', 78789933, 'lovelycare12@hotmail.com');
+
+#provee
+insert into provee(id_Producto, id_Proveedor, cantidad) values
+(33, 201, 50),
+(34, 202, 10),
+(35, 203, 40),
+(36, 204, 100),
+(37, 205, 30);
+
